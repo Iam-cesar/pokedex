709c0f995505a7a423479d93492f4674a971272f
cesar augusto <cesaraugustojun@gmail.com>
2021-11-28 14:02:09 -0300

    add footer

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,7 +1,19 @@
 import { cardConfig } from 'components/UI/mixins'
+import { $white } from 'components/UI/colors'
 import styled from 'styled-components'
 
 export const FooterContainer = styled.footer`
   ${cardConfig}
+  display: grid;
   grid-area: footer;
+  place-items: center;
+
+  a{
+    color: ${$white};
+    text-decoration: none;
+
+    :hover{
+      text-decoration: underline;
+    }
+  }
 `
