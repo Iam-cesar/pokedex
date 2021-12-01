c3b3557aa0a0e681a94834677a8e504b51a933c2
cesar augusto <cesaraugustojun@gmail.com>
2021-12-01 09:02:44 -0300

    code review

--- src/components/PokemonList/index.jsx
+++ src/components/PokemonList/index.jsx
@@ -1,22 +1,23 @@
 import { PokemonListContainer } from './style'
 import React, { useEffect, useState } from 'react'
-import Api from 'Api'
+import Pokemon from 'api/Pokemon'
 import { usePokemon } from 'hooks/usePokemon'
 import pokeball from 'assets/svg/pokeball.svg'
 
 function PokemonList () {
   const { setPokemon } = usePokemon()
   const [pokemonList, setPokemonList] = useState([])
+  const Apipokemon = new Pokemon()
 
   useEffect(() => {
     handlePokemonsList()
   }, [])
 
   async function handlePokemonsList () {
-    const res = await Api.getPokemonList()
+    const res = await Apipokemon.getGroup()
     const container = []
     await Promise.all(res.map(async (item) => {
-      const pokemon = await Api.getPokemonFullInfo(item)
+      const pokemon = await Apipokemon.getAllInformation(item)
       container.push(pokemon)
     }))
     setPokemonList(container)
