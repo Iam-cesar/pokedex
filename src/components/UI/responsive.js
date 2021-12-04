774f9d266492938db35a0f24fbc9bdc06c9b072d
cesar augusto <cesaraugustojun@gmail.com>
2021-12-04 13:36:23 -0300

    add responsivity

--- src/components/UI/responsive.js
+++ src/components/UI/responsive.js
@@ -1,5 +1,18 @@
-export const screen400px = `
+export const mainContent650px = `
+  @media screen and (max-width: 650px){
+    flex-direction: column;
+
+    div{
+      width: 100%;
+    }
+  }
 `
 
-export const screen800px = `
+export const typesAndStats450px = `
+@media screen and (max-width: 450px){
+  .pokemon__type--container div p,
+  .pokemon__type--double--container div p{
+    font-size: 1.1rem !important;
+  }
+}
 `
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,8 +1,4 @@
 import styled from 'styled-components'
-import {
-  screen400px,
-  screen800px
-} from 'components/UI/responsive'
 
 export const HomeContainer = styled.main`
   display: flex;
@@ -11,6 +7,4 @@ export const HomeContainer = styled.main`
   gap: 20px;
   margin: 1rem;
 
-  ${screen400px}
-  ${screen800px}
 `
